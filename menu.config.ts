aa2bd715ca5f92282f65c17ed4c530427c5d0b85
technovitaSolution <technovitasolution@gmail.com>
2025-08-12 08:42:53 +0530

    refactor: intialized the project with the template

---
+++ menu.config.ts
@@ -0,0 +1,12 @@
+// Define the menu items
+export const mainMenu = {
+  home: "/",
+  about: "https://job4youth.com",
+  blog: "/posts",
+};
+
+export const contentMenu = {
+  categories: "/posts/categories",
+  tags: "/posts/tags",
+  authors: "/posts/authors",
+};
